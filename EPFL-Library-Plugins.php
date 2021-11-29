abeaafbfc8094f01ac2b994f5cd0fb4f32c8cc74
Sylvain Vuilleumier <sylvain.vuilleumier@epfl.ch>
2021-11-29 14:12:17 +0100

    Changement de serveur + test si un param est déjà existant dans l'url.

--- EPFL-Library-Plugins.php
+++ EPFL-Library-Plugins.php
@@ -8,7 +8,7 @@ Description:
     and get external content from this external source according to the
     transmitted parameters.
     2: Automatically inserts the Beast box in the Library pages
-Version: 1.5
+Version: 1.6
 Author: Raphaël REY & Sylvain VUILLEUMIER
 Author URI: https://people.epfl.ch/raphael.rey
 Author URI: https://people.epfl.ch/sylvain.vuilleumier
@@ -84,7 +84,13 @@ function epfl_library_external_content_process_shortcode($attributes, $content =
 
     // Test the final concatened url
     if (external_content_urlExists($url)) {
-        $response = wp_remote_get($url . '?' . $_SERVER['QUERY_STRING']);
+        if (strpos($url, '?') !== false) {
+			$response = wp_remote_get($url . '&' . $_SERVER['QUERY_STRING']);
+		}
+		else {
+			$response = wp_remote_get($url . '?' . $_SERVER['QUERY_STRING']);
+		}
+
         $page = $response['body'];
         return $page;
     } else {
@@ -135,7 +141,7 @@ UTILISATION:
 
 function epfl_library_beast_redirect_process_shortcode($attributes, $content = null){
     return '<script>
-"use strict";function Url_redirect(e,r){var t=this,n=r;t.params_to_analyse=Object.keys(n.params);var s=function(e){var r=e.indexOf("//")+2,t=e.indexOf("/",r),n=e.indexOf("?",t);return-1===n&&(t=e.length),r>=2&&t>=0?e.substring(t,n):""},a=function(e){var r=e.indexOf("?");return-1!==r?e.substring(r):""},i=function(e){for(var r=0;r<e.length;r++)e[r].match(/^\d{3,9}$/)&&(e[r].length<9&&(e[r]="0".repeat(9-e[r].length).concat(e[r])),e[r]="ebi01_prod".concat(e[r]));return e},l=function(e){for(var r="",t=0;t<e.length;t++)0===t?r=e[0]:r+="+OR+"+e[t];return r};t.url_src={url:e},Object.defineProperty(t.url_src,"search",{get:function(){return a(t.url_src.url)}}),Object.defineProperty(t.url_src,"pathname",{get:function(){return s(t.url_src.url)}}),Object.defineProperty(t,"paramsList",{get:function(){var e=[];if(t.url_src.search.length>0)for(var r=t.url_src.search.substring(1),n=r.split("&"),s=0;s<n.length;s++){var a=n[s].split("=");if(a.length>1){var i=a[0],l=a[1].split(",");e.push({key:i,values:l})}}return e}}),Object.defineProperty(t,"lang",{get:function(){for(var e=0;e<n.lang.length;e++)if(t.url_src.pathname.indexOf(n.lang[e].test)>-1&&!1===n.lang[e].default)return e;return n.lang.length-1}}),t.url_dest={},Object.defineProperty(t.url_dest,"key",{get:function(){for(var e=0;e<t.paramsList.length;e++)if(t.params_to_analyse.indexOf(t.paramsList[e].key)>-1)return t.paramsList[e].key;return null}}),Object.defineProperty(t.url_dest,"values",{get:function(){for(var e=0;e<t.paramsList.length;e++)if(t.paramsList[e].key===t.url_dest.key){var r=t.paramsList[e].values;return"record"===t.url_dest.key&&(r=i(r)),r}return[]}}),Object.defineProperty(t.url_dest,"path",{get:function(){return l(t.url_dest.values)}}),t.getDestUrl=function(){var e=n.default_url;return t.url_dest.key&&t.url_dest.path&&(e=n.params[t.url_dest.key]+t.url_dest.path),e+=n.lang[t.lang].path}}const PATTERNS={default_url:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?vid=41SLSP_EPF:prod",params:{isbn:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&facet=rtype,include,books&query=isbn,contains,",record:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_DN_CI&search_scope=DN_and_CI&vid=41SLSP_EPF:prod&query=any,contains,",query:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&query=any,contains,",issn:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&query=issn,contains,",fulltext:"https://kissrv117.epfl.ch/beast/redirect?nebis_id="},lang:[{name:"ed",test:"/edit/",path:"",default:!1},{name:"en",test:"/en/",path:"&prefLang=en",default:!1},{name:"default",test:"",path:"",default:!0}]};var link=new Url_redirect(window.location.href,PATTERNS);0!==link.lang&&(window.location.href=link.getDestUrl());
+"use strict";function Url_redirect(e,r){var t=this,n=r;t.params_to_analyse=Object.keys(n.params);var s=function(e){var r=e.indexOf("//")+2,t=e.indexOf("/",r),n=e.indexOf("?",t);return-1===n&&(t=e.length),r>=2&&t>=0?e.substring(t,n):""},a=function(e){var r=e.indexOf("?");return-1!==r?e.substring(r):""},i=function(e){for(var r=0;r<e.length;r++)e[r].match(/^\d{3,9}$/)&&(e[r].length<9&&(e[r]="0".repeat(9-e[r].length).concat(e[r])),e[r]="ebi01_prod".concat(e[r]));return e},l=function(e){for(var r="",t=0;t<e.length;t++)0===t?r=e[0]:r+="+OR+"+e[t];return r};t.url_src={url:e},Object.defineProperty(t.url_src,"search",{get:function(){return a(t.url_src.url)}}),Object.defineProperty(t.url_src,"pathname",{get:function(){return s(t.url_src.url)}}),Object.defineProperty(t,"paramsList",{get:function(){var e=[];if(t.url_src.search.length>0)for(var r=t.url_src.search.substring(1),n=r.split("&"),s=0;s<n.length;s++){var a=n[s].split("=");if(a.length>1){var i=a[0],l=a[1].split(",");e.push({key:i,values:l})}}return e}}),Object.defineProperty(t,"lang",{get:function(){for(var e=0;e<n.lang.length;e++)if(t.url_src.pathname.indexOf(n.lang[e].test)>-1&&!1===n.lang[e].default)return e;return n.lang.length-1}}),t.url_dest={},Object.defineProperty(t.url_dest,"key",{get:function(){for(var e=0;e<t.paramsList.length;e++)if(t.params_to_analyse.indexOf(t.paramsList[e].key)>-1)return t.paramsList[e].key;return null}}),Object.defineProperty(t.url_dest,"values",{get:function(){for(var e=0;e<t.paramsList.length;e++)if(t.paramsList[e].key===t.url_dest.key){var r=t.paramsList[e].values;return"record"===t.url_dest.key&&(r=i(r)),r}return[]}}),Object.defineProperty(t.url_dest,"path",{get:function(){return l(t.url_dest.values)}}),t.getDestUrl=function(){var e=n.default_url;return t.url_dest.key&&t.url_dest.path&&(e=n.params[t.url_dest.key]+t.url_dest.path),e+=n.lang[t.lang].path}}const PATTERNS={default_url:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?vid=41SLSP_EPF:prod",params:{isbn:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&facet=rtype,include,books&query=isbn,contains,",record:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_DN_CI&search_scope=DN_and_CI&vid=41SLSP_EPF:prod&query=any,contains,",query:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&query=any,contains,",issn:"https://slsp-epfl.primo.exlibrisgroup.com/discovery/search?tab=41SLSP_EPF_MyInst_and_CI&search_scope=MyInst_and_CI&vid=41SLSP_EPF:prod&query=issn,contains,",fulltext:"https://sisbdev.epfl.ch/beast/redirect?nebis_id="},lang:[{name:"ed",test:"/edit/",path:"",default:!1},{name:"en",test:"/en/",path:"&prefLang=en",default:!1},{name:"default",test:"",path:"",default:!0}]};var link=new Url_redirect(window.location.href,PATTERNS);0!==link.lang&&(window.location.href=link.getDestUrl());
 </script>';
 }
 add_shortcode('epfl_library_beast_redirect', 'epfl_library_beast_redirect_process_shortcode');
@@ -149,16 +155,16 @@ function insert_beastbox($content) {
 
 		if (function_exists('pll_current_language')) {
 			if ( pll_current_language() == 'fr' ) {
-				$content .= do_shortcode( "[remote_content url='https://kissrv117.epfl.ch/beast/searchbox']" ) ;
+				$content .= do_shortcode( "[remote_content url='https://sisbdev.epfl.ch/beast/searchbox']" ) ;
 
 			}
 			else {
-				$content .= do_shortcode( "[remote_content url='https://kissrv117.epfl.ch/beast/searchbox?lang=en']" );
+				$content .= do_shortcode( "[remote_content url='https://sisbdev.epfl.ch/beast/searchbox?lang=en']" );
 			}
 		}
 		// Default French
 		else {
-			$content .= do_shortcode( "[remote_content url='https://kissrv117.epfl.ch/beast/searchbox']" );
+			$content .= do_shortcode( "[remote_content url='https://sisbdev.epfl.ch/beast/searchbox']" );
 		}
 	}
 	return $content . "<script>
